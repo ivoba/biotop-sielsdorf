c8eabdc2085a5fff79a2256f51fa7127dded338d
Ivo Bathke <ivo.bathke@gmail.com>
2022-08-16 09:19:56 +0200

    base

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,6 +3,7 @@ import { defineConfig } from 'astro/config';
 // https://astro.build/config
 export default defineConfig({
 	site: `https://biotop-sielsdorf.github.io`,
+	base: `/biotop-sielsdorf`,
 	legacy: {
 		astroFlavoredMarkdown: true
 	},
