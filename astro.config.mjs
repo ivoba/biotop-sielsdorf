2903232b8c630c91d8e3d0e671d169fb621a676d
Ivo Bathke <ivo.bathke@gmail.com>
2022-08-16 11:23:28 +0200

    base fix

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,7 +3,7 @@ import { defineConfig } from 'astro/config';
 // https://astro.build/config
 export default defineConfig({
 	site: `https://ivoba.github.io`,
-	base: `/biotop-sielsdorf`,
+	base: `/`,
 	legacy: {
 		astroFlavoredMarkdown: true
 	},
