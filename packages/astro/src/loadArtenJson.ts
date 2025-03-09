2dcb86c9ac47939db63da7eead2eafd3e86a3297
Ivo Bathke <ivo.bathke@gmail.com>
2025-03-09 11:49:25 +0100

    improved arten navigation

--- packages/astro/src/loadArtenJson.ts
+++ packages/astro/src/loadArtenJson.ts
@@ -1,11 +1,14 @@
 import * as fs from "fs";
 import { Specie } from "./types";
 
-export async function loadData(jsonFile: string): Promise<{ species: Specie[], location: {} }> {
+export async function loadData(jsonFile: string): Promise<{ species: Record<string, Record<string, Specie>>, location: {}, count: number }> {
   if (fs.existsSync(jsonFile) === false) {
     console.error(jsonFile + " not found");
     return;
   }
 
-  return JSON.parse(fs.readFileSync(jsonFile).toString());
+  const data = JSON.parse(fs.readFileSync(jsonFile).toString()) as { species: Record<string, Record<string, Specie>>, location: {} };
+  const totalCount = Object.values(data.species).reduce((sum, group) => sum + Object.keys(group).length, 0);
+  
+  return { ...data, count: totalCount };
 }
